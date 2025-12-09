5695f6a9dc3fe4ed1b4c79541221a32bece97532
Денис Колодешников <prostodeniskdt@gmail.com>
2025-12-09 21:13:56 +0000

    А чтоб подключить аи помощника нужен будет апи ключ который нужен будет

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,13 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import {googleCloud} from '@genkit-ai/google-cloud';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
+    }),
+    googleCloud(),
+  ],
   model: 'googleai/gemini-2.5-flash',
 });

c03fca97d21cbb0a5d065b416df6620263e8d016
Денис Колодешников <prostodeniskdt@gmail.com>
2025-12-09 21:27:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7924133843-7f552",
+  "appId": "1:1074195698844:web:8053af5dbb73c80d13c959",
+  "apiKey": "AIzaSyBlCZ6QnauA_WA6ec5kjJMRpqW9DO_MUgs",
+  "authDomain": "studio-7924133843-7f552.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1074195698844"
+};

5b5ca7ca04d5773aae0bf209e26548511c679250
Денис Колодешников <prostodeniskdt@gmail.com>
2025-12-10 10:20:49 +0000

    Try fixing this error: `Console Error: Invalid prop `data-state` supplie

--- src/components/sessions/inventory-table.tsx
+++ src/components/sessions/inventory-table.tsx
@@ -115,7 +115,7 @@ export function InventoryTable({ lines, setLines, products, isEditable }: Invent
                     </TableRow>
                     {Object.entries(subCategories).map(([subCategory, lines]) => (
                          <Collapsible asChild key={subCategory} defaultOpen>
-                            <TableBody>
+                            <>
                                 {subCategory !== 'uncategorized' && (
                                     <TableRow className="bg-muted/10 hover:bg-muted/20">
                                          <TableCell colSpan={9} className="py-2">
@@ -170,7 +170,7 @@ export function InventoryTable({ lines, setLines, products, isEditable }: Invent
                                         ))}
                                     </>
                                 </CollapsibleContent>
-                            </TableBody>
+                            </>
                         </Collapsible>
                     ))}
                  </TableBody>

3f6992e0be5c84a750c9b4e1e736953522bd1457
Денис Колодешников <prostodeniskdt@gmail.com>
2025-12-11 10:08:30 +0000

    Мы близки скорее всего сейчас такая проблема

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,9 @@
+'use server';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
-    }),
+    googleAI(),
   ],
 });

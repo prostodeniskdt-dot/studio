69f5265d5a65e37b6e7445be3dda98a6a40d57dd
Денис Колодешников <prostodeniskdt@gmail.com>
2025-12-12 19:00:04 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    allowedDevOrigins: ['*.cloudworkstations.dev'],
+    
   },
+  allowedDevOrigins: ['*.cloudworkstations.dev'],
   typescript: {
     ignoreBuildErrors: true,
   },

4fb041877978feac661311d9732667c3310e0d52
Денис Колодешников <prostodeniskdt@gmail.com>
2025-12-13 18:07:34 +0000

    Теперь добавиь графику каких-то цифр чтоб они так же уменьшались и увели

--- src/components/login-animation.tsx
+++ src/components/login-animation.tsx
@@ -19,6 +19,13 @@ export function LoginAnimation({ className }: { className?: string }) {
         <rect className="bar bar-3" x="40" y="80" width="18" height="0" />
         <rect className="bar bar-4" x="60" y="80" width="18" height="0" />
         <rect className="bar bar-5" x="80" y="80" width="18" height="0" />
+
+        {/* Animated Numbers */}
+        <text className="bar-text bar-text-1" x="9" y="80">25k</text>
+        <text className="bar-text bar-text-2" x="29" y="80">42k</text>
+        <text className="bar-text bar-text-3" x="49" y="80">18k</text>
+        <text className="bar-text bar-text-4" x="69" y="80">35k</text>
+        <text className="bar-text bar-text-5" x="89" y="80">29k</text>
       </svg>
     </div>
   );

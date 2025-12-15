edc8f47f8c167d3773a8f476f368278781c7f74a
Денис Колодешников <prostodeniskdt@gmail.com>
2025-12-15 18:52:02 +0000

    Пришли еще Фрагмент src/components/user-nav.tsx целиком вокруг getIniti

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -56,9 +56,9 @@ export function UserNav() {
     if (!name) return '??';
     const names = name.split(' ');
     if (names.length > 1 && names[1]) {
-      return names[0][0] + names[names.length - 1][0];
+      return (names[0][0] + names[names.length - 1][0]).toUpperCase();
     }
-    return names[0][0];
+    return name[0].toUpperCase();
   };
 
   if (isUserLoading) {

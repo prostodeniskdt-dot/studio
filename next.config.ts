d3d204aa5b0de5b619bb6a79d7e535f028fc25ff
Денис Колодешников <prostodeniskdt@gmail.com>
2025-12-18 18:20:22 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-import { withSentryConfig } from '@sentry/nextjs';
 import type {NextConfig} from 'next';
 import withPWA from 'next-pwa';
 
@@ -146,9 +145,6 @@ const pwaConfig = withPWA({
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    instrumentationHook: true, // Enable instrumentation hook for Sentry
-  },
   images: {
     remotePatterns: [
       {
@@ -173,52 +169,7 @@ const nextConfig: NextConfig = {
   },
 };
 
-// Wrap the Next.js config with PWA and Sentry configs
+// Wrap the Next.js config with PWA config
 const configWithPWA = pwaConfig(nextConfig);
 
-export default withSentryConfig(
-  configWithPWA,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: process.env.SENTRY_ORG,
-    project: process.env.SENTRY_PROJECT,
-    
-    // Only upload source maps in production
-    widenClientFileUpload: true,
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: true,
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for better error tracking
-    widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    tunnelRoute: '/monitoring',
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-  }
-);
+export default configWithPWA;

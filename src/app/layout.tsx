e27a41c2f1943df990d56c74a7c2cd05d55177dc
prostodeniskdt-dot <prostodeniskdt@gmail.com>
2026-05-06 21:55:42 +0300

    Описание изменений

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
@@ -27,11 +27,12 @@ export const metadata: Metadata = {
   },
 };
 
-export function generateViewport() {
-  return {
-    themeColor: '#000000',
-  };
-}
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
+  themeColor: '#000000',
+};
 
 export default function RootLayout({
   children,
